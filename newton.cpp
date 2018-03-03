c2c3b86f5a43e083f13eec7097f54b8bcb01ee97
Jigar Patel <jigar.moyo@gmail.com>
2018-03-02 19:53:24 -0600

    Update newton.cpp
    Initialized array & variables. Created loop to handle game

--- newton.cpp
+++ newton.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
-using namespace std;
-
 int main() {
-  for(int i= 0; i <= 7; i++){
-          
-    for(int j = i; j <= i + 32; j = j+8){
-      cout << j << " ";
-    }
-      cout << endl;
-  }
-  
+    const int SIZE = 40; // FIXME : NEEDS TO CHANGE OTHER PART OF PROGRAM
+    char array[SIZE];
+    char column_X;
+    char column_rx_num = '0';
+    char column_O;
+    char column_ro_num = '0';
+    int movecount = 0;
+    
+    for(int i = 0; i <= SIZE; i++){ array[i] = '.'; }
 
-  
+    student_game_info();
+    
+    do{
+        moveXInput(column_X, movecount, array, column_rx_num, SIZE);
+        cout << endl;
+        checkWinner(array, SIZE);
+        moveOInput(column_O, movecount, array, column_ro_num, SIZE);
+        checkWinner(array, SIZE);
+    }while(true);
+    
+    return 0;
 }
